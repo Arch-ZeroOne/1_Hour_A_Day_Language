d573c013aa0fb097a3d4b5072447f40f3ed4456a
Windyl Monton <windyl1.monton.lvhs@gmail.com>
2025-05-13 09:46:57 +0800

    Finished day-1

---
+++ typescript/note.md
@@ -0,0 +1,16 @@
+
+Coding along with : https://www.youtube.com/watch?v=3mDny9XAgic&t=1172s
+
+Intallation:
+npm install -g typescript
+
+Generates the tsconfig file
+
+Initialization:
+tsc --init
+
+We need to transcript the file before running to prevent the error
+
+tsc type_annotations.ts // compiles the typescript file
+
+node type_annotations.js // runs the compiled typescript file
---
+++ typescript/type_annotations.js
@@ -0,0 +1,25 @@
+/*
+//*Lets typescript know its a number
+let id: number = 5;
+//*Lets typescript know its a String
+let company: string = "Acme Corporation";
+//*Lets typescript know its a boolean
+let isPublished: boolean = false;
+
+//*Defines an array of numbers
+let ids: number[] = [1, 2, 3, 4, 5];
+
+
+//*Defines the value as any of the type
+let ex: any = "Pedro";
+let xArr: any[] = ["Pedro", 0, true];
+*/
+var a = "Hello";
+var b = "World";
+//(a: string, b: string) defines the value to be expected in the function which in this case is string
+// (:string) defines the return type of the functions
+var concatenateValues = function (a, b) {
+    return 3;
+};
+console.log(concatenateValues("Hello", "World")); //Hello Wrold
+console.log(concatenateValues("5", "10")); //15
