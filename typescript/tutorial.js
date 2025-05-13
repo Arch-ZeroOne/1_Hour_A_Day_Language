4e1c7c3ad495e94ccf2a6bcbe380f150e1e38316
Windyl Monton <windyl1.monton.lvhs@gmail.com>
2025-05-13 10:05:40 +0800

    Learned Interfaces

--- typescript/tutorial.js
+++ typescript/tutorial.js
@@ -1,3 +1,4 @@
+//! Type Annotaions
 /*
 //*Lets typescript know its a number
 let id: number = 5;
@@ -13,15 +14,33 @@ let ids: number[] = [1, 2, 3, 4, 5];
 //*Defines the value as any of the type
 let ex: any = "Pedro";
 let xArr: any[] = ["Pedro", 0, true];
-*/
+
 const a = "Hello";
 const b = "World";
 
-//(a: string, b: string) defines the value to be expected in the function which in this case is string
-// (:string) defines the return type of the functions
+(a: string, b: string) defines the value to be expected in the function which in this case is string
+(:string) defines the return type of the functions
 const concatenateValues = (a: string, b: string): string => {
   return a + b;
 };
 
 console.log(concatenateValues("Hello", "World")); //Hello Wrold
 console.log(concatenateValues("5", "10")); //15
+
+*/
+//Follows the roadmap or blueprint from the UserInterface interface
+var User = {
+    id: 2,
+    name: "Pedro",
+    greet: function (message) {
+        console.log(message);
+    },
+};
+//Defines the type for objects
+if (!User.age) {
+    console.log("User has no age");
+}
+else {
+    console.log(User.age);
+}
+console.log(User.greet("Hello"));
