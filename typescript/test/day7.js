22f5976f9fa74bc8981844343ea6461c7204dc7a
Windyl Monton <windyl1.monton.lvhs@gmail.com>
2025-06-03 14:25:26 +0800

    Done day7

---
+++ typescript/test/day7.js
@@ -0,0 +1,3 @@
+var float_number = 0.45;
+var string_word = "Windyl";
+console.log("".concat(float_number, " ").concat(string_word));
---
+++ typescript/test/day7.ts
@@ -0,0 +1,4 @@
+const float_number: number = 0.45;
+const string_word: string = "Windyl";
+
+console.log(`${float_number} ${string_word}`);
